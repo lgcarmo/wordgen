ea8b166cd19a7b3e8ff77db17da635a67622f4ac
lgcarmo <48776615+lgcarmo@users.noreply.github.com>
2020-04-07 19:03:28 -0300

    Upadate 2.1
    Capitalize -c upper
    maria += Maria

--- wordgen.py
+++ wordgen.py
@@ -6,54 +6,25 @@ import random
 from random import randrange
 import itertools
 
+parser=argparse.ArgumentParser (description='Requests.')
+parser.add_argument ('-w', dest='k_vetor', help='lista dado:dados:senhas', required=True)
+parser.add_argument ('-m', dest='k_min', help='minum 0', required=True)
+parser.add_argument ('-M', dest='k_max', help='Max X', required=True)
+parser.add_argument ('-c', dest='k_cap', help='Captalize', required=False)
 
-parser = argparse.ArgumentParser(description='Requests.')
-parser.add_argument('-w', dest='k_vetor', help='lista dado:dados:senhas', required=True)
-parser.add_argument('-m', dest='k_min', help='minum 0', required=True)
-parser.add_argument('-M', dest='k_max', help='Max X', required=True)
+args=parser.parse_args ()
 
+n=(int (args.k_min))
+m=(int (args.k_max))
 
-
-
-args = parser.parse_args()
-
-n =(int(args.k_min))
-m =(int(args.k_max))
-
-chrs =(args.k_vetor).split(':')
+chrs=(args.k_vetor).split (':')
 
 while n <= m:
-	n+=1
-	for xz in itertools.product(chrs, repeat=n):
-		print('').join(xz)
-	#	print(args.k_num)
+    n+=1
+    for xz in itertools.product (chrs, repeat=n):
+        print ('').join (xz)
+    #	print(args.k_num)
 
-	#!/usr/bin/python
-
-import argparse
-import sys
-import random
-from random import randrange
-import itertools
-
-
-parser = argparse.ArgumentParser(description='Requests.')
-parser.add_argument('-w', dest='k_vetor', help='lista dado:dados:senhas', required=True)
-parser.add_argument('-m', dest='k_min', help='minum 0', required=True)
-parser.add_argument('-M', dest='k_max', help='Max X', required=True)
-
-
-
-
-args = parser.parse_args()
-
-n =(int(args.k_min))
-m =(int(args.k_max))
-
-chrs =(args.k_vetor).split(':')
-
-while n <= m:
-	n+=1
-	for xz in itertools.product(chrs, repeat=n):
-		print('').join(xz)
-	#	print(args.k_num)
+    for xz in itertools.product (chrs, repeat=n):
+        if args.k_cap == 'upper':
+            print ('').join (xz).capitalize()
