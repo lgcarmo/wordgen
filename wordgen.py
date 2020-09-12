d13d4e105cc03553d8aeee6c8484f832ee75d728
lgcarmo <48776615+lgcarmo@users.noreply.github.com>
2020-09-12 12:07:50 -0300

    Update wordgen.py

--- wordgen.py
+++ wordgen.py
@@ -5,12 +5,20 @@ import sys
 import random
 from random import randrange
 import itertools
+from pyleet import Leet
+
+################################
+#K40S
+#lgcarmo@protonmail.com
+################################
 
 parser=argparse.ArgumentParser (description='Requests.')
 parser.add_argument ('-w', dest='k_vetor', help='lista dado:dados:senhas', required=True)
-parser.add_argument ('-m', dest='k_min', help='minum 0', required=True)
-parser.add_argument ('-M', dest='k_max', help='Max X', required=True)
+parser.add_argument ('-m', dest='k_min', help='minum 0', default=0, required=False)
+parser.add_argument ('-M', dest='k_max', help='Max X', default=2, required=False)
 parser.add_argument ('-c', dest='k_cap', help='Captalize', required=False)
+parser.add_argument ('-l', '--leet', help='parameter -l leet', dest='k_leet', action='store_true', required=False)
+parser.add_argument ('-o', '--output', type=argparse.FileType(mode='w'), dest='k_output', help='Salva a wordlist em um arquivo.')
 
 args=parser.parse_args ()
 
@@ -22,9 +30,34 @@ chrs=(args.k_vetor).split (':')
 while n <= m:
     n+=1
     for xz in itertools.product (chrs, repeat=n):
-        print ('').join (xz)
-    #	print(args.k_num)
+        if args.k_output is not None:
+            list= ('').join (xz)
+            args.k_output.write(list + '\n')
+        else:
+            print ('').join(xz)
 
     for xz in itertools.product (chrs, repeat=n):
         if args.k_cap == 'upper':
-            print ('').join (xz).capitalize()
+            if args.k_output is not None:
+                upperlist=('').join (xz).capitalize()
+                args.k_output.write(upperlist + '\n')
+            else:
+
+                print('').join (xz).capitalize()
+
+    for xz in itertools.product (chrs, repeat=n):
+        if args.k_leet:
+            if args.k_output is not None:
+                leet=('').join (xz)
+                t=Leet (leet)
+                tra = []
+                tra.append(t.translate())
+                for trans in tra:
+                    args.k_output.write(trans + '\n')
+
+            else:
+                leet=('').join (xz)
+                t=Leet (leet)
+                print(t.translate())
+
+
